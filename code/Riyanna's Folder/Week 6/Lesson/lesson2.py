842d43af948868151b9588e3528db8cde9f19c8e
Rykan-ce <reancedelempalmado@gmail.com>
2026-03-19 08:56:51 +0800

    Add files via upload

---
+++ code/Riyanna's Folder/Week 6/Lesson/lesson2.py
@@ -0,0 +1,54 @@
+
+
+#list of dictionary
+data = [
+    {"item":"Fish","category":"Food","subcategory":"Seafood","price":120,"qt_sold_june":400,"qt_sold_july":425},
+    {"item":"Beef","category":"Food","subcategory":"Meat","price":400,"qt_sold_june":250,"qt_sold_july":300},
+    {"item":"Egg","category":"Food","subcategory":"Dairy & Eggs","price":10,"qt_sold_june":4125,"qt_sold_july":4500},
+    {"item":"Pen","category":"Non-Food","subcategory":"Stationery","price":20,"qt_sold_june":312,"qt_sold_july":401},
+]
+
+
+print('-----------------[1] printing all elements------------------')
+
+#for item in data:
+#   print(item)
+
+# turn into range
+for item in range(0, len(data)):
+    print(data[item], end=' ')
+
+print('\n\n')
+
+print('-----------------[2] printing name part------------------')
+
+#for item in data:
+#    print(item['item'])
+
+for item in range(0, len(data)):
+    print(data[item]['item'],end='\n')
+
+print('\n\n')
+
+print('-----------------[3] getting june sales------------------')
+
+#for item in data:
+#    print(item['item'], ': ', item['price']*item['qt_sold_june'])
+
+for item in range(0, len(data)):
+    print(data[item]['item'], ': ', data[item]['price']*data[item]['qt_sold_june'], end='\n')
+
+
+
+
+
+
+
+
+
+
+
+
+
+# create the for-in-range equivalent of the above code
+
